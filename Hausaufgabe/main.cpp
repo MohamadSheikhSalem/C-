7030f939189a32e7597538cbdb6d7bcb52e46d12
Majed Abdalrahman <ml.expert22@gmail.com>
2020-05-20 10:09:45 +0200

    Initial Commit

---
+++ Hausaufgabe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hausaufgabe
+//
+//  Created by Majed Abdalrahman on 20.05.20.
+//  Copyright © 2020 Majed Abdalrahman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
